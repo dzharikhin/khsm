544ee9df9b2f7f24002bd29dc19f17fd6faa7ebb
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2018-03-27 14:25:29 +0300

    add flush task for testing purpose

---
+++ flush_data.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from tzlocal import get_localzone
+
+import loggers
+import service
+
+logger = loggers.logging.getLogger(__name__)
+
+
+def task():
+    service.init()
+    service.reset_data()
+
+
+if __name__ == "__main__":
+    sched = BlockingScheduler(timezone=get_localzone())
+    sched.add_job(task, CronTrigger.from_crontab('*/5 * * * *'))
+    sched.start()
+
